48bfd69237fffb4481b71efd893987cfb26ea004
hu.po <hugo.p.cmu@gmail.com>
2023-10-04 09:10:22 -0500

    moar

---
+++ utils.py
@@ -0,0 +1,12 @@
+def q2rot():
+    # quaternion to rotation matrix
+    # normalize quaternion
+    pass
+
+def project():
+    # project gaussians to screenspace
+    pass
+
+def alphablend():
+    # alpha blend gaussians together
+    pass
