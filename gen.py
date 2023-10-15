7097ecdb0b25e702b9e2cdd08d6db75655c35e44
hu.po <hugo.p.cmu@gmail.com>
2023-10-15 15:12:30 -0500

    gpt prompts

--- gen.py
+++ gen.py
@@ -6,7 +6,9 @@ import urllib.request
 from PIL import Image
 from io import BytesIO
 
-def generate_images(prompts: list = [], height: int = 1024, width: int = 1024, save_images: bool = False) -> np.ndarray:
+def generate_images(prompts: list = [], 
+                    height: int = 1024, 
+                    width: int = 1024) -> list:
     outputs = []
     # Generate a unique id for this generation session
     session_id = uuid.uuid4()
@@ -32,22 +34,28 @@ def generate_images(prompts: list = [], height: int = 1024, width: int = 1024, s
         with urllib.request.urlopen(output[0]) as url:
             f = BytesIO(url.read())
         img = Image.open(f)
-        img_array = np.array(img)
-        outputs.append(img_array)
 
-        # Save the image locally if save_images is True
-        if save_images:
-            save_path = os.path.join(os.getenv('DATA_DIR', '.'), f'{session_id}_{i+1}of{len(prompts)}.png')
-            img.save(save_path)
+        # Save the image locally
+        save_path = os.path.join(os.getenv('DATA_DIR', '.'), f'{session_id}_{i+1}of{len(prompts)}.png')
+        img.save(save_path)
+        outputs.append((save_path, prompt))
 
-    # Stack all images into a single numpy tensor
-    return np.stack(outputs, axis=0)
+    return outputs
 
 if __name__ == "__main__":
+    subject = "white bengal cat"  # You can replace this with any other subject
     # Test the function locally
     prompts = [
-        "3/4 portrait of cat facing left",
-        "3/4 portrait of cat facing right"
+        f"Dynamic 3/4 view of {subject} facing left with a clear emphasis on depth and perspective.",
+        f"Sharp 3/4 view of {subject} facing right, capturing the subtle contours and details.",
+        f"Direct frontal view of {subject}, offering a symmetrical and straightforward perspective.",
+        f"Top-down aerial view of {subject}, showcasing the subject from a bird's-eye perspective.",
+        f"Distinct profile view of {subject} facing left, highlighting the side silhouette.",
+        f"Isometric view of {subject}, creating a three-dimensional effect.",
+        f"Low-angle view of {subject}, looking up, emphasizing its dominance and stature.",
+        f"High-angle view of {subject}, looking down, making the subject appear smaller or more vulnerable.",
+        f"Rear view of {subject}, focusing on the back and hindquarters, capturing a unique perspective.",
+        f"Diagonal front-left view of {subject}, blending both frontal and profile features.",
     ]
     result = generate_images(prompts, height=512, width=512, save_images=True)
     print(result)
